b7f7586678cfd76f7cdbe2e4878733f762fc5c14
hyunwookim-rsk <hyunwoo.kim@rakuten.com>
2022-07-15 15:03:14 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ const port = 3000
 
 
 /**
- * @api {get}  Hello World
+ * @api {get} / Hello_World Server
  * @apiName Hello world app
- * @apiGroup User
+ * @apiGroup Backend
  *
  * @apiSuccess {String} Return Hello World message.
  */
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+/**
+ * @api {get} /status Check_status_of_server Server
+ * @apiName App status
+ * @apiGroup Backend
+ *
+ * @apiSuccess {String} Return ok message.
+ */
+ app.get('/status', (req, res) => {
+  res.send('ok!')
 })
 
 app.listen(port, () => {
