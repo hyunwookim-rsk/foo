042149c305c8eebd024ccacea6a9de5038663058
Kim, Hyunwoo | RSK <hyunwoo.kim@rakuten.com>
2022-07-21 17:05:51 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 const { Sequelize } = require('sequelize');
 const { initModels } = require('./models/init-models');
